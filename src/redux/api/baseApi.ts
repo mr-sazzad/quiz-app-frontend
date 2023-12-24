7363b874a6b6d2f168d16c74a47806c2ec6c74b9
mr-sazzad <main.sazzad@gmail.com>
2023-12-24 13:03:30 +0600

    url modified

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { tagTypeLists } from "./tagTypes";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: "https://quiz-app-two-wine-93.vercel.app//api/v1",
+    baseUrl: "https://quiz-app-two-wine-93.vercel.app/api/v1",
   }),
   endpoints: (builder) => ({}),
   //  result cashing
