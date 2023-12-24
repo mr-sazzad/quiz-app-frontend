0e8917089c55c40160172b86640c778cf5c7f696
mr-sazzad <main.sazzad@gmail.com>
2023-12-24 12:58:54 +0600

    profile part commited

---
+++ src/app/profile/edit/[id]/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import Loading from "@/components/Loading";
+import CategoriesBanner from "@/components/admins/categories/CategoriesBanner";
+import Footer from "@/components/footer/Footer";
+import { UploadImageToImageBB } from "@/helpers/imageUpload";
+import {
+  useGetSingleUserQuery,
+  useUpdateSingleUserMutation,
+} from "@/redux/api/users/userApi";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+const UpdateProfile = () => {
+  const title = "Update Profile";
+  const description =
+    "keep updating your profile and getting updates for your website";
+
+  const { id } = useParams();
+  const { data: user, isLoading } = useGetSingleUserQuery(id);
+  const [updateSingleUser] = useUpdateSingleUserMutation();
+  const { register, handleSubmit } = useForm();
+  const [loading, setLoading] = useState(false);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  const handleProfileUpdate = async (data: any) => {
+    let profileImage = null;
+    try {
+      setLoading(true);
+
+      if (data.file[0]) {
+        profileImage = await UploadImageToImageBB(data.file[0]);
+      }
+
+      const requestedData = {
+        name: data.name || user.name,
+        phone: data.phone || user.phone,
+        bio: data.bio || user.bio,
+        image: profileImage || user.image,
+        age: data.age || user.age,
+      };
+
+      const result = await updateSingleUser(requestedData);
+      setLoading(false);
+      console.log(result);
+    } catch (err: any) {
+      setLoading(false);
+      console.log(err);
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div>
+      <CategoriesBanner title={title} description={description} />
+      <div className="px-[30px] md:px-[50px]">
+        {/* update user info */}
+        <div className="flex justify-center items-center mt-7 mb-10">
+          <form
+            className="p-5 border border-gray-400 rounded"
+            onSubmit={handleSubmit(handleProfileUpdate)}
+          >
+            <p className="text-center text-xl font-medium">Update Profile</p>
+            <div className="flex flex-col gap-1 ">
+              <label className="ml-2">Name</label>
+              <input
+                type="text"
+                defaultValue={user?.name}
+                {...register("name")}
+                className="px-3 py-2 rounded border border-gray-400 outline-none md:w-[400px] w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-1 mt-2">
+              <label className="ml-2">email</label>
+              <input
+                type="text"
+                defaultValue={user?.email}
+                disabled
+                className="px-3 py-2 rounded border border-gray-400 outline-none md:w-[400px] w-full cursor-not-allowed"
+              />
+            </div>
+            <div className="flex flex-col gap-1 mt-2">
+              <label className="ml-2">Phone</label>
+              <input
+                type="text"
+                {...register("phoneNumber")}
+                defaultValue={user?.phoneNumber}
+                className="px-3 py-2 rounded border border-gray-400 outline-none md:w-[400px] w-full"
+              />
+            </div>
+            <div className="flex flex-col gap-1 mt-2">
+              <label className="ml-2">Age</label>
+              <input
+                type="text"
+                defaultValue={user?.age}
+                {...register("age")}
+                className="px-3 py-2 rounded border border-gray-400 outline-none md:w-[400px] w-full"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1 mt-2">
+              <label className="ml-2">Image</label>
+              <input
+                type="file"
+                {...register("file")}
+                className="px-3 py-2 rounded border border-gray-400 outline-none md:w-[400px] w-full"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1 mt-2">
+              <label className="ml-2">Bio</label>
+              <input
+                type="text"
+                {...register("bio")}
+                defaultValue={user?.bio}
+                className="px-3 py-2 rounded border border-gray-400 outline-none md:w-[400px] w-full"
+              />
+            </div>
+            <div>
+              <button
+                className={`mt-3 px-3 py-2 rounded bg-green-400 hover:bg-green-500 transition w-full ${
+                  loading ? "cursor-not-allowed" : ""
+                }`}
+                type="submit"
+              >
+                {loading ? "Loading ..." : "Update Information"}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+        <Footer />
+    </div>
+  );
+};
+
+export default UpdateProfile;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import Loading from "@/components/Loading";
+import CategoriesBanner from "@/components/admins/categories/CategoriesBanner";
+import Footer from "@/components/footer/Footer";
+import { getUserFromLocalStorage } from "@/helpers/jwt";
+import { useGetSingleUserQuery } from "@/redux/api/users/userApi";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Profile = () => {
+  const title = "Your Profile";
+  const description =
+    "keep updating your profile and getting updates for your website";
+  const router = useRouter();
+
+  const currentUser = getUserFromLocalStorage() as any;
+
+  useEffect(() => {
+    if (!currentUser) {
+      router.push("/sign-in");
+    }
+  });
+
+  const { data: user, isLoading } = useGetSingleUserQuery(currentUser?.id);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <CategoriesBanner title={title} description={description} />
+      <div className="px-[30px] md:px-[50px]">
+        <div className="flex justify-center items-center">
+          <div className="flex flex-col md:w-[500px] w-full border border-gray-400 rounded mt-5 p-5 mb-10">
+            {/* profile image */}
+            <div className="flex justify-center">
+              <div className="relative w-[70px] h-[70px] p-1 border-2 border-gray-400 rounded-full">
+                <Image
+                  src="/assets/cover.png"
+                  alt="profile-image"
+                  fill
+                  priority
+                  className="rounded-full"
+                />
+              </div>
+            </div>
+            {/* name and bio */}
+            <div>
+              <h2 className="text-xl font-medium text-center">{user?.name}</h2>
+              <p className="text-sm text-gray-500 text-center">
+                {user?.bio ? user?.bio : "please update your bio"}
+              </p>
+            </div>
+
+            {/* information */}
+            <p className="text-lg text-gray-600 font-medium mt-5">
+              Your Information
+            </p>
+            <div className="flex flex-col gap-2">
+              <p className="px-3 py-2 rounded border border-gray-400">
+                Email: {user?.email}
+              </p>
+              <p className="px-3 py-2 rounded border border-gray-400">
+                Phone: {user?.phone ? user?.phone : "Update your number"}
+              </p>
+              <p className="px-3 py-2 rounded border border-gray-400">
+                Age: {user?.age ? user?.age : "Update your age"}
+              </p>
+            </div>
+
+            {/* Quiz Info */}
+            <div className="mt-3">
+              <p className="text-lg text-gray-600 font-medium">
+                Your Activities
+              </p>
+              <p className="px-3 py-2 rounded border border-gray-400">
+                Your Score:
+                <span className="ml-1">{user?.score}</span>
+              </p>
+              {/* attend questions */}
+              <p className="px-3 py-2 rounded border border-gray-400 mt-2">
+                Total attend:
+                <span className="ml-1">{user?.totalQuestion}</span>
+              </p>
+            </div>
+
+            <div>
+              <button
+                className="px-4 py-2 rounded bg-green-400 hover:bg-green-500 transition mt-3 w-full"
+                onClick={() => router.push(`/profile/edit/${user?.id}`)}
+              >
+                Edit your profile
+              </button>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
